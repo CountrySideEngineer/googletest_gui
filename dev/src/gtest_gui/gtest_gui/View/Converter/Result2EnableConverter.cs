33de08c2669db5bac9c7deef05566bb42b093801
CountrySideEngineer <deep-sleeper@nifty.com>
2021-05-16 06:41:06 +0900

    Update view to hide buttons about history of test. -Add classes to convert a result of test into property of button, isEnable and Visibility. -Update view xaml file adding the convert property.

---
+++ dev/src/gtest_gui/gtest_gui/View/Converter/Result2EnableConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace gtest_gui.View.Converter
+{
+	[ValueConversion(typeof(bool), typeof(string))]
+	public class Result2EnableConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string result = value as string;
+			string resultLower = result.ToLower();
+			bool isEnabled = false;
+			if ((string.IsNullOrEmpty(result)) || (string.IsNullOrWhiteSpace(result)))
+			{
+				isEnabled = false;
+			}
+			else
+			{
+				isEnabled = true;
+			}
+			return isEnabled;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
---
+++ dev/src/gtest_gui/gtest_gui/View/Converter/Result2VisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace gtest_gui.View.Converter
+{
+	[ValueConversion(typeof(Visibility), typeof(string))]
+	public class Result2VisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			Visibility visible = Visibility.Hidden;
+			string result = value as string;
+			string resultLower = result.ToLower();
+			if ((string.IsNullOrEmpty(result)) || (string.IsNullOrWhiteSpace(result)))
+			{
+				visible = Visibility.Hidden;
+			}
+			else
+			{
+				visible = Visibility.Visible;
+			}
+			return visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
