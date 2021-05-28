09d2cae2f84a259edcc45d26f919163932ce29df
CountrySideEngineer <deep-sleeper@nifty.com>
2021-05-29 06:15:30 +0900

    Add view to show detail test data. -Add view and view model of detail test data, history of the test.

---
+++ dev/src/gtest_gui/gtest_gui/ViewModel/TestHistoryViewModel.cs
@@ -0,0 +1,39 @@
+﻿using gtest2html;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.ViewModel
+{
+	public class TestHistoryViewModel : ViewModelBase
+	{
+		/// <summary>
+		/// Field of test cases.
+		/// </summary>
+		protected IEnumerable<TestCase> _testCases;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestHistoryViewModel()
+		{
+			this._testCases = null;
+		}
+
+		/// <summary>
+		/// List of test case, TestCase object.
+		/// </summary>
+		public IEnumerable<TestCase> TestCases
+		{
+			get
+			{
+				return this._testCases;
+			}
+			set
+			{
+				this._testCases = value;
+				this.RaisePropertyChanged(nameof(this.TestCases));
+			}
+		}
+	}
+}
