9685d145205c21c3213e63cbccf31b736c0fd6d7
CountrySideEngineer <deep-sleeper@nifty.com>
2021-05-29 07:27:17 +0900

    Update view and view model class of test history view. -Update view of test history adding area to show test name and its result. -Update view model class adding field and property about test name and its result.

--- dev/src/gtest_gui/gtest_gui/ViewModel/TestHistoryViewModel.cs
+++ dev/src/gtest_gui/gtest_gui/ViewModel/TestHistoryViewModel.cs
@@ -8,6 +8,16 @@ namespace gtest_gui.ViewModel
 	public class TestHistoryViewModel : ViewModelBase
 	{
 		/// <summary>
+		/// Test name field.
+		/// </summary>
+		protected string _testName;
+
+		/// <summary>
+		/// Test result field.
+		/// </summary>
+		protected string _testResult;
+
+		/// <summary>
 		/// Field of test cases.
 		/// </summary>
 		protected IEnumerable<TestCase> _testCases;
@@ -17,6 +27,8 @@ namespace gtest_gui.ViewModel
 		/// </summary>
 		public TestHistoryViewModel()
 		{
+			this.TestName = string.Empty;
+			this.TestResult = string.Empty;
 			this._testCases = null;
 		}
 
@@ -33,6 +45,37 @@ namespace gtest_gui.ViewModel
 			{
 				this._testCases = value;
 				this.RaisePropertyChanged(nameof(this.TestCases));
+			}
+		}
+
+		/// <summary>
+		/// Tes name property.
+		/// </summary>
+		public string TestName
+		{
+			get
+			{
+				return this._testName;
+			}
+			set
+			{
+				this._testName = value;
+				this.RaisePropertyChanged(nameof(TestName));
+			}
+		}
+
+		/// <summary>
+		/// Test result property.
+		/// </summary>
+		public string TestResult
+		{
+			get
+			{
+				return this._testResult;
+			}
+			set
+			{
+				this.RaisePropertyChanged(nameof(TestResult));
 			}
 		}
 	}
