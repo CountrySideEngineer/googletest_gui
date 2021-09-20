00ce8d951a2a2c00a386977bc9e5057b25ca8e60
CountrySideEngineer <deep-sleeper@nifty.com>
2021-09-20 17:42:36 +0900

    Add not committed file.

---
+++ dev/src/gtest_gui/gtest_gui/Model/TestHistoryReader.cs
@@ -0,0 +1,61 @@
+﻿using gtest2html;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace gtest_gui.Model
+{
+	public class TestHistoryReader : TestResultReader
+	{
+		public new IEnumerable<TestCase> ReadTest(TestInformation testInfo)
+		{
+			try
+			{
+				string testFileName = Path.GetFileNameWithoutExtension(testInfo.TestFile);
+				IEnumerable<string> testResultFiles = this.GetTestResultFiles(testFileName);
+				IEnumerable<TestCase> srcTestCases = this.GetAllTestCases(testResultFiles);
+				IEnumerable<TestCase> testCases = this.ExtractTestCase(testInfo, srcTestCases);
+
+				return testCases;
+			}
+			catch (DirectoryNotFoundException)
+			{
+				throw;
+			}
+			catch (Exception ex)
+			when ((ex is IndexOutOfRangeException) || (ex is NullReferenceException))
+			{
+				return null;
+			}
+		}
+
+		protected virtual IEnumerable<TestCase> ExtractTestCase(TestInformation testInfo, IEnumerable<TestCase> srcCases)
+		{
+			try
+			{
+				var testItem = testInfo.TestItems.ElementAt(0);
+				var classAndTestName = testItem.Name.Split(".");
+				string className = classAndTestName[0];
+				string testName = classAndTestName[1];
+				var testCases = srcCases.Where(_ =>
+					_.Name.Equals(testName) && _.ClassName.Equals(className));
+
+				return testCases;
+			}
+			catch (Exception ex)
+			when ((ex is IndexOutOfRangeException) || (ex is NullReferenceException))
+			{
+				throw;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+
+				throw ex;
+			}
+		}
+	}
+}
+ 
