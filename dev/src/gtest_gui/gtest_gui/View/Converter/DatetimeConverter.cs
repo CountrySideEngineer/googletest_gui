fd47a0ed0752ad0d934df84eb4dac27321d4def3
CountrySideEngineer <deep-sleeper@nifty.com>
2023-02-10 07:56:52 +0900

    Add converter. Add a class to convert DateTime class to string with a format when display the date time.

---
+++ dev/src/gtest_gui/gtest_gui/View/Converter/DatetimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace gtest_gui.View.Converter
+{
+	[ValueConversion(typeof(DateTime), typeof(string))]
+	public class DatetimeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			DateTime dateTime = (DateTime)value;
+			string converted = dateTime.ToString("yyyy/MM/dd HH:mm.ss");
+
+			return converted;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
