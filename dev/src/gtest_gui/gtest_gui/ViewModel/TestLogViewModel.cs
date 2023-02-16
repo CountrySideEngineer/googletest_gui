a1c30b07babbb2a2546f3ff5f62fdd0df251aa80
CountrySideEngineer <deep-sleeper@nifty.com>
2023-02-17 06:10:57 +0900

    Add view and view model of test log window. Start developing log window.

---
+++ dev/src/gtest_gui/gtest_gui/ViewModel/TestLogViewModel.cs
@@ -0,0 +1,71 @@
+﻿using CountrySideEngineer.ViewModel.Base;
+using gtest_gui.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.ViewModel
+{
+	public class TestLogViewModel : ViewModelBase
+	{
+		/// <summary>
+		/// Path to log file to display.
+		/// </summary>
+		protected string _logFilePath;
+
+		/// <summary>
+		/// Field of window title.
+		/// </summary>
+		protected string _windowTitle;
+
+		/// <summary>
+		/// Logs loaded from file specified by _logFilePath;
+		/// </summary>
+		protected string _logContent;
+
+		/// <summary>
+		/// Test information data.
+		/// </summary>
+		protected TestInformation _testInfo;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestLogViewModel()
+		{
+
+		}
+
+		/// <summary>
+		/// Window title property.
+		/// </summary>
+		public string WindowTitle
+		{
+			get
+			{
+				return _logFilePath;
+			}
+			set
+			{
+				_logFilePath = value;
+				RaisePropertyChanged(nameof(WindowTitle));
+			}
+		}
+
+		/// <summary>
+		/// Log content property.
+		/// </summary>
+		public string LogContent
+		{
+			get
+			{
+				return _logContent;
+			}
+			set
+			{
+				_logContent = value;
+				RaisePropertyChanged(nameof(LogContent));
+			}
+		}
+	}
+}
