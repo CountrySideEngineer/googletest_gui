ad9f141a09a99b2e561d3cea5e22feb0d63e0601
CountrySideEngineer <deep-sleeper@nifty.com>
2023-02-17 06:33:17 +0900

    Add mover of test log window.

---
+++ dev/src/gtest_gui/gtest_gui/MoveWindow/Move2TestLog.cs
@@ -0,0 +1,19 @@
+﻿using gtest_gui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.MoveWindow
+{
+	public class Move2TestLog : IMoveWindow
+	{
+		/// <summary>
+		/// Move to test log window.
+		/// </summary>
+		/// <param name="srcContext">Data to show in the window.</param>
+		public void Move(object srcContext)
+		{
+			var viewModel = (TestHistoryViewModel)srcContext;
+		}
+	}
+}
