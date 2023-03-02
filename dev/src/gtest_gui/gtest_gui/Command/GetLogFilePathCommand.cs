979a1577014c0f604b75c6b067512a46e9308585
CountrySideEngineer <deep-sleeper@nifty.com>
2023-03-03 07:09:27 +0900

    Add GetLogFilePathCommand, command class to get log file path.

---
+++ dev/src/gtest_gui/gtest_gui/Command/GetLogFilePathCommand.cs
@@ -0,0 +1,47 @@
+﻿using gtest_gui.Command.Argument;
+using gtest_gui.Model;
+using gtest2html;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace gtest_gui.Command
+{
+	public class GetLogFilePathCommand : ITestCommand
+	{
+		protected OutputDirAndFile _outptuDirFile = null;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public GetLogFilePathCommand()
+		{
+			string currentDir = Directory.GetCurrentDirectory();
+			_outptuDirFile = new OutputDirAndFile(currentDir);
+		}
+
+		/// <summary>
+		/// Execute command to get log file path.
+		/// </summary>
+		/// <param name="cmdArgument"></param>
+		/// <returns></returns>
+		public object ExecuteCommand(TestCommandArgument cmdArgument)
+		{
+			TestCase testCase = cmdArgument.TestCase;
+			string testFilePath = cmdArgument.TestInfo.TestFile;
+			string testFileName = Path.GetFileNameWithoutExtension(testFilePath);
+
+			_outptuDirFile.TestExeFileName = testFileName;
+			_outptuDirFile.TestTimeStamp = testCase.Timestamp;
+			var reader = new TestLogReader()
+			{
+				OutputDirFile = _outptuDirFile,
+				TestCase = testCase
+			};
+			string logFilePath = reader.GetLogFilePath();
+
+			return logFilePath;
+		}
+	}
+}
