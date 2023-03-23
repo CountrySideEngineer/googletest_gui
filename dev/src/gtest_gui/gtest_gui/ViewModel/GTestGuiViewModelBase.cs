aebb770da1549c5465a7a88338288497b2946f4a
CountrySideEngineer <deep-sleeper@nifty.com>
2023-03-23 22:08:39 +0900

    Add base view model class. Add new view model class to be a common base of the application.

---
+++ dev/src/gtest_gui/gtest_gui/ViewModel/GTestGuiViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using CountrySideEngineer.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.ViewModel
+{
+	public class GTestGuiViewModelBase : ViewModelBase
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public GTestGuiViewModelBase(): base() { }
+
+		public virtual void RaiseNotifyErrorEvent(EventArgs e)
+		{
+
+		}
+
+		public virtual void RaiseNotifySuccessEvent(EventArgs e)
+		{
+
+		}
+
+		protected virtual void ShowErrorDialog()
+		{
+
+		}
+
+		/// <summary>
+		/// Move to a dialog, window, to show a result.
+		/// </summary>
+		protected virtual void Move2ResultDialog() { }
+	}
+}
