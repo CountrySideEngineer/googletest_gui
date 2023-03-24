15621016a5273ce05c29cf247ac89d951c1f93cb
CountrySideEngineer <deep-sleeper@nifty.com>
2023-03-25 06:42:00 +0900

    Add a class about exception. Add CommandException class, handling exception occurred while command exception.

---
+++ dev/src/gtest_gui/gtest_gui/Command/Exception/CommandException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.Command.Exception
+{
+	public class CommandException : System.Exception
+	{
+		/// <summary>
+		/// Default exception
+		/// </summary>
+		public CommandException() : base() { }
+
+		public CommandException(System.Exception innerException) : base(string.Empty, innerException) { }
+	}
+}
