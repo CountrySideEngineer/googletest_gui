a2eab161ee3494f64ac551475d9a7ca93dcd17d2
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-03 06:50:05 +0900

    Add LoadTestFromGoogleTestCommand class. A class which inherits LaodTestCommand and uses GoogleTestListReader class to read available test list.

---
+++ dev/src/gtest_gui/gtest_gui/Command/LoadTestFromGoogleTestCommand.cs
@@ -0,0 +1,18 @@
+﻿using gtest_gui.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.Command
+{
+	public class LoadTestFromGoogleTestCommand : LoadTestCommand
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public LoadTestFromGoogleTestCommand() : base()
+		{
+			ListReader = new GoogleTestListReader();
+		}
+	}
+}
