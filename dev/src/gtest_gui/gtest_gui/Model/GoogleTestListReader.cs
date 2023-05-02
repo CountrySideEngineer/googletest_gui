2154f1c6e07b2df967c17c3caa8ae51db9868a03
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-03 06:37:38 +0900

    Add class to get test list. Add GoogleTestListReader class to get available test list from test program using googletest framework.

---
+++ dev/src/gtest_gui/gtest_gui/Model/GoogleTestListReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace gtest_gui.Model
+{
+	public class GoogleTestListReader : TestListReader
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public GoogleTestListReader() : base() { }
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="path">Path to file to get the list of test to execute.</param>
+		public GoogleTestListReader(string path)  :  base(path) { }
+
+		/// <summary>
+		/// Get ProcessStartInfo object to be used in process to read process from application using googletest framework.
+		/// </summary>
+		/// <param name="fileName">Path to file to run.</param>
+		/// <returns>ProcessStartInfo object</returns>
+		protected override ProcessStartInfo CreateProcStartInfo(string fileName)
+		{
+			try
+			{
+				var info = base.CreateProcStartInfo(fileName);
+				info.Arguments = "--gtest_list_tests";
+
+				return info;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+}
